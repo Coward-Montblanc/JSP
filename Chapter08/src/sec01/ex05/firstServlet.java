42f151e1008679685154d259c79d45d2556c67ba
Coward-Montblanc <127381991+Coward-Montblanc@users.noreply.github.com>
2023-03-30 16:17:44 +0900

    Add files via upload

---
+++ Chapter08/src/sec01/ex05/firstServlet.java
@@ -0,0 +1,30 @@
+package sec01.ex05;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class firstServlet
+ */
+@WebServlet("/first5")
+public class firstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+
+		RequestDispatcher dispatch = request.getRequestDispatcher("second5");
+		dispatch.forward(request, response);
+	}
+
+}
---
+++ Chapter08/src/sec01/ex06/firstServlet.java
@@ -0,0 +1,30 @@
+package sec01.ex06;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class firstServlet
+ */
+@WebServlet("/first6")
+public class firstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+
+		RequestDispatcher dispatch = request.getRequestDispatcher("second6?name=kim");
+		dispatch.forward(request, response);
+	}
+
+}
